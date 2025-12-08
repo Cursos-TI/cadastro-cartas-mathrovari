5558f5c0f8513e477f8d9935eb7618966620493c
mathrovari <mathcustodio@gmail.com>
2025-12-08 20:25:06 +0000

    Foram realizadas mudanças para o tema 3 - novato
    As seguintes mudanças foram implementadas:
    - troca do scanf pelo fgets para cidade
    - inclusão de função de limpeza de buffer
    - inclusão de código para limpar pulo de linha lido pelo stdin
    - melhorias realizadas nos comentários
    - area de comentários das atividades anteriores
    - resolução da atividade novato do tema 3

--- super_trunfo.c
+++ super_trunfo.c
@@ -31,6 +31,16 @@ char *carta(int carta1){
     return "Carta 2";
 }
 
+// Limpar entrada do buffer para usar o fgets
+// Remove caracteres restantes no stdin (como '\n' do scanf) antes de usar fgets
+// Excerto pelo primeiro fgets, usar sempre antes de usar fgets novamente
+void limpar_buffer(void) {
+    int descartar;
+    while ((descartar = getchar()) != '\n' && descartar != EOF) {
+        ; 
+    }
+}
+
 
 // Inicio do programa --------------------------------------------
 
@@ -38,9 +48,10 @@ int main()
 {
     printf("\n\n"); //Pulo estético
 
+    //------------------------------------------------------------------------------
     // Declaração de váriaveis para o cadastro de cartas do jogo super trunfo de paises
-    char estado1='A',cidade1[]="exemplo",codCarta1[]="Z00";
-    char estado2='A',cidade2[]="exemplo",codCarta2[]="Z00";
+    char estado1='A',cidade1[100]="",codCarta1[5]="Z00";
+    char estado2='A',cidade2[100]="",codCarta2[5]="Z00";
 
     unsigned long int populacao1=123456,populacao2=123456;
     int pontos_turisticos1=2,pontos_turisticos2=2;
@@ -49,21 +60,31 @@ int main()
     float KMquadrado2=1.5,pib2=123.34, pibpercapita2=0, densidadep2=0;
     float super1=0, super2=0;
 
+    // Declaração de váriaveis de calculos
+    float pibbilhao1 = pib1*1000000000; // Tranformando o PIB normal em bilhão
+    float pibbilhao2 = pib2*1000000000; // Tranformando o PIB normal em bilhão
 
+    float inversodensidade1 = 1/densidadep1; // Calculo de densidade inversa para Super Poder
+    float inversodensidade2 = 1/densidadep2; // Calculo de densidade inversa para Super Poder
+
+    //------------------------------------------------------------------------------
     // Introdução ao Jogo
+
     printf("Olá jogador! Estamos criando um jogo chamado Super Trunfo de Paises");
     printf("e preciso da sua ajuda para cadastrar duas cartas desse jogo.");
-    printf("\nApós o cadastro das cartas vou compara-las e te mostrar o vencedor\n");
+    printf("\nApós o cadastro das cartas vou comparar um atributo e te mostrar o vencedor\n");
 
     // Entrada de dados primeira carta
     printf("\nIniciaremos cadastrando a carta nº1");
-    printf("\nQual nome da cidade que deseja cadastrar: ");
-    scanf("%s",cidade1);
-    printf("\n\n %s ? Boa escolha!\n",cidade1);
+    printf("\nQual nome da cidade que deseja cadastrar: \n");
+    //scanf("%s",cidade1); -- antiga entrada de dados do tipo string para cidade1
+    fgets(cidade1,sizeof(cidade1),stdin);
+    cidade1[strcspn(cidade1, "\n")] = '\0'; // remover pulo de linha criado por fgets
+    printf("\n\nBoa escolha!\n");
     printf("Agora escolha uma letra de A a H que representará o estado dessa cidade: \n");
-    scanf(" %c",&estado1);
+    scanf("%c",&estado1);
     printf("\nAgora escolha um código para a carta, precisam ser 2 numeros, por exemplo '00':\n");
-    scanf("%s",&codCarta1);
+    scanf("%s",codCarta1);
     printf("\nQuantos habitantes existem nessa cidade?\n");
     scanf("%lu",&populacao1);
     printf("\nQual seria o tamanho dessa cidade em kilometros quadrados?\n");
@@ -76,12 +97,15 @@ int main()
     // Entrada de dados segunda carta
     printf("\n\nFinalizamos a carta nº1, agora vamos cadastrar a carta nº2");
     printf("\nQual nome da cidade que deseja cadastrar:\n");
-    scanf("%s",cidade2);
-    printf("\n\n %s ? Novamente uma boa escolha!\n",cidade2);
+    //scanf("%s",cidade2); -- antiga entrada de dados do tipo string para cidade2
+    limpar_buffer();
+    fgets(cidade2,sizeof(cidade2),stdin);
+    cidade2[strcspn(cidade2, "\n")] = '\0'; // remover pulo de linha criado por fgets
+    printf("\n\nNovamente uma boa escolha!\n");
     printf("Agora escolha uma letra de A a H que representará o estado dessa cidade: \n");
-    scanf(" %c",&estado2);
+    scanf("%c",&estado2);
     printf("\nAgora escolha um código para a carta, precisam ser 2 numeros, por exemplo '00':\n");
-    scanf("%s",&codCarta2);
+    scanf("%s",codCarta2);
     printf("\nQuantos habitantes existem nessa cidade?\n");
     scanf("%lu",&populacao2);
     printf("\nQual seria o tamanho dessa cidade em km²?\n");
@@ -91,17 +115,18 @@ int main()
     printf("\nAgora ultima pergunta. Quantos pontos turisticos tem essa cidade?\n");
     scanf("%d",&pontos_turisticos2);
 
+    //------------------------------------------------------------------------------
     //Calculos
-    float pibbilhao1 = pib1*1000000000; // Tranformando o PIB normal em bilhão
-    float pibbilhao2 = pib2*1000000000; // Tranformando o PIB normal em bilhão
 
+    // Caclulo de densidade
     densidadep1 = divisao(populacao1,KMquadrado1);
     densidadep2 = divisao(populacao2,KMquadrado2);
+
+    // Calculo de PIB Percapita
     pibpercapita1 = pibbilhao1/ (float) populacao1;
     pibpercapita2 = pibbilhao2/ (float) populacao2;
 
-    float inversodensidade1 = 1/densidadep1; // Calculo de densidade inversa para Super Poder
-    float inversodensidade2 = 1/densidadep2; // Calculo de densidade inversa para Super Poder
+    // Calculo de super poder
     super1 = (float)populacao1+(float)pontos_turisticos1+KMquadrado1+pib1+pibpercapita1+inversodensidade1;
     super2 = (float)populacao2+(float)pontos_turisticos2+KMquadrado2+pib2+pibpercapita2+inversodensidade2; 
 
@@ -123,27 +148,33 @@ int main()
     char *WinCartaPIBPerc = carta(WinPontuacaoPIBPerc);
     char *WinCartaSuper = carta(WinPontuacaoSuper);
 
+    //------------------------------------------------------------------------------
+
     // Mostrar Vencedores 
-    printf("\n\nChegou o grande momento de saber quem venceu, veja a comparação a baixo");
-    printf("\nComparação das Cartas:\n");
-    printf("\nPoplação: %s Venceu (%d)",WinCartaPopulacao,WinPontuacaoPopulacao);
-    printf("\nÁrea: %s Venceu (%d)",WinCartaKMquadrado,WinPontuacaoKMquadrado);
-    printf("\nPIB: %s Venceu (%d)",WinCartaPIB,WinPontuacaoPIB);
-    printf("\nPontos Turisticos: %s Venceu (%d)",WinCartaPontosT,WinPontuacaoPontosT);
-    printf("\nDensidade Populacional: %s Venceu (%d)",WinCartaDensidade,WinPontuacaoDensidade);
-    printf("\nPIB per Capita: %s Venceu (%d)",WinCartaPIBPerc,WinPontuacaoPIBPerc);
-    printf("\nSuper Poder: %s Venceu (%d)",WinCartaSuper,WinPontuacaoSuper);
-
     printf("\n\n");
+    printf("\nComparação das Cartas (Atributo: Pontos Turisticos):");
+    printf("\nCarta 1 - %s: %d tem pontos turisticos",cidade1,pontos_turisticos1);
+    printf("\nCarta 2 - %s: %d tem pontos turisticos",cidade2,pontos_turisticos2);
 
-    // ---------------------------------------------------------------------------
-    // ------------Comentário atividade Novato e Aventureiro-------------------
-    // Estou comentando a demostração de dados das cartas dado não saber se era para ficar
-    // ativo. Ela esta funcional e foi ajustado para a ativdade Mestre, caso precise
+    if (WinPontuacaoPontosT == 1)
+    {
+        printf("\nResultado: %s (%s) Venceu",WinCartaPontosT,cidade1);
+    }else{
+        printf("\nResultado: %s (%s) Venceu",WinCartaPontosT,cidade2);
+    }
 
-    /*
+    //Finalização
+    printf("\n\nMuito Obrigado por jogar conosco");
+    printf("\nAté mais!");
 
-    printf("\n\nAgora que sabe o vencedor, vou deixar a baixo os dados das cartas que cadastrou");
+    printf("\n\n"); //Pular linha de forma estética
+    return 0;
+}
+
+/*  -----------Comentário Atividade Novato e Aventureiro Tema 2-------------------
+    ------- A baixo está a demonstração final da atividade notavo de aventureiro do tema 2 ----------
+
+    printf("\n\nOlhe a baixo as cartas que cadastrou");
 
     printf("\n\nCarta 1:\n");
     printf("Estado: %c\n",estado1);
@@ -169,14 +200,30 @@ int main()
     printf("PIB per Capita: R$%.2f reais\n",pibpercapita2);
     printf("Super Poder: %.0f",super2);
 
-    */
+    //Finalização
+    printf("\n\nMuito Obrigado por nos ajudar a montar esse jogo");
+    printf("\nAté mais!");
 
-    // ----------------------------------------------------------------------------
+*/
+
+/*  -----------Comentário Atividade Avançado Tema 2-------------------
+    ------- A baixo está a demonstração final da atividade avançado do tema 2 ----------
+
+    // Mostrar Vencedores 
+    printf("\n\nChegou o grande momento de saber quem venceu, veja a comparação a baixo");
+    printf("\nComparação das Cartas:\n");
+    printf("\nPoplação: %s Venceu (%d)",WinCartaPopulacao,WinPontuacaoPopulacao);
+    printf("\nÁrea: %s Venceu (%d)",WinCartaKMquadrado,WinPontuacaoKMquadrado);
+    printf("\nPIB: %s Venceu (%d)",WinCartaPIB,WinPontuacaoPIB);
+    printf("\nPontos Turisticos: %s Venceu (%d)",WinCartaPontosT,WinPontuacaoPontosT);
+    printf("\nDensidade Populacional: %s Venceu (%d)",WinCartaDensidade,WinPontuacaoDensidade);
+    printf("\nPIB per Capita: %s Venceu (%d)",WinCartaPIBPerc,WinPontuacaoPIBPerc);
+    printf("\nSuper Poder: %s Venceu (%d)",WinCartaSuper,WinPontuacaoSuper);
+
+    printf("\n\n");
 
     //Finalização
     printf("\n\nMuito Obrigado por jogar conosco");
     printf("\nAté mais!");
 
-    printf("\n\n"); //Pular linha de forma estética
-    return 0;
-}
+*/
