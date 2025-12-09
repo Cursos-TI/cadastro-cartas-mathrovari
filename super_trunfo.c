bd514d735b38d0599368acb693df250ed3ae227a
mathrovari <mathcustodio@gmail.com>
2025-12-09 19:35:17 +0000

    Foi implementado desafio do Tema 3 - Aventureiro Usando do - while e switch foi criado código no vscode do linux zorin compilado no testado e compilado no github.

--- super_trunfo.c
+++ super_trunfo.c
@@ -13,6 +13,8 @@ float divisao(float n1, float n2){
 int maiorganha(double carta1, double carta2){
     if(carta1 > carta2){
         return 1;
+    }else if(carta1 == carta2){
+        return 2;
     }
     return 0;
 }
@@ -20,6 +22,8 @@ int maiorganha(double carta1, double carta2){
 int menorganha(double carta1, double carta2){
     if(carta1 < carta2){
         return 1;
+    }else if(carta1 == carta2){
+        return 2;
     }
     return 0;
 }
@@ -27,6 +31,8 @@ int menorganha(double carta1, double carta2){
 char *carta(int carta1){
     if(carta1 == 1){
         return "Carta 1";
+    }else if(carta1 == 2){
+        return "Empate";
     }
     return "Carta 2";
 }
@@ -50,14 +56,15 @@ int main()
 
     //------------------------------------------------------------------------------
     // Declaração de váriaveis para o cadastro de cartas do jogo super trunfo de paises
-    char estado1='A',cidade1[100]="",codCarta1[5]="Z00";
-    char estado2='A',cidade2[100]="",codCarta2[5]="Z00";
+    char estado1='A',cidade1[100]="São Paulo",codCarta1[5]="A01";
+    char estado2='B',cidade2[100]="Rio de Janeiro",codCarta2[5]="B02";
 
-    unsigned long int populacao1=123456,populacao2=123456;
-    int pontos_turisticos1=2,pontos_turisticos2=2;
+    int populacao1=250100,populacao2=500672;
+    int pontos_turisticos1=30,pontos_turisticos2=30;
+    int opcao=0;
 
-    float KMquadrado1=1.5,pib1=123.34, pibpercapita1=0, densidadep1=0;
-    float KMquadrado2=1.5,pib2=123.34, pibpercapita2=0, densidadep2=0;
+    float KMquadrado1=348.45,pib1=9801.22, pibpercapita1=0, densidadep1=0;
+    float KMquadrado2=194.15,pib2=6729.10, pibpercapita2=0, densidadep2=0;
     float super1=0, super2=0;
 
     // Declaração de váriaveis de calculos
@@ -68,13 +75,158 @@ int main()
     float inversodensidade2 = 1/densidadep2; // Calculo de densidade inversa para Super Poder
 
     //------------------------------------------------------------------------------
+    //Calculos
+
+    // Caclulo de densidade
+    densidadep1 = divisao(populacao1,KMquadrado1);
+    densidadep2 = divisao(populacao2,KMquadrado2);
+
+    // Calculo de PIB Percapita
+    pibpercapita1 = pibbilhao1/ (float) populacao1;
+    pibpercapita2 = pibbilhao2/ (float) populacao2;
+
+    // Calculo de super poder
+    super1 = (float)populacao1+(float)pontos_turisticos1+KMquadrado1+pib1+pibpercapita1+inversodensidade1;
+    super2 = (float)populacao2+(float)pontos_turisticos2+KMquadrado2+pib2+pibpercapita2+inversodensidade2; 
+
+    //Comparação das Cartas
+    int WinPontuacaoPopulacao = maiorganha(populacao1,populacao2);
+    int WinPontuacaoKMquadrado = maiorganha(KMquadrado1,KMquadrado2);
+    int WinPontuacaoPIB = maiorganha(pibbilhao1,pibbilhao2);
+    int WinPontuacaoPontosT = maiorganha(pontos_turisticos1,pontos_turisticos2);
+    int WinPontuacaoDensidade = menorganha(densidadep1,densidadep2);
+    int WinPontuacaoPIBPerc = maiorganha(pibpercapita1,pibpercapita2);
+    int WinPontuacaoSuper = maiorganha(super1,super2);
+
+    //Definicição de vencedores
+    char *WinCartaPopulacao = carta(WinPontuacaoPopulacao);
+    char *WinCartaKMquadrado = carta(WinPontuacaoKMquadrado);
+    char *WinCartaPIB = carta(WinPontuacaoPIB);
+    char *WinCartaPontosT = carta(WinPontuacaoPontosT);
+    char *WinCartaDensidade = carta(WinPontuacaoDensidade);
+    char *WinCartaPIBPerc = carta(WinPontuacaoPIBPerc);
+    char *WinCartaSuper = carta(WinPontuacaoSuper);
+
+    //------------------------------------------------------------------------------
     // Introdução ao Jogo
 
-    printf("Olá jogador! Estamos criando um jogo chamado Super Trunfo de Paises");
-    printf("e preciso da sua ajuda para cadastrar duas cartas desse jogo.");
-    printf("\nApós o cadastro das cartas vou comparar um atributo e te mostrar o vencedor\n");
+    printf("Olá jogador! Aqui você vai jogar Super Trunfo de Paises");
+    printf("\nEscolha escolha o atributo que vamos comparar:\n");
+    
+    do
+    {
 
-    // Entrada de dados primeira carta
+        printf("1- População\n");
+        printf("2- Área\n");
+        printf("3- PIB\n");
+        printf("4- Pontos Turisticos\n");
+        printf("5- Densidade Populacional\n");
+        printf("ATENÇÃO: Digite apenas o numero\n");
+        scanf("%d",&opcao);
+
+        switch (opcao)
+        {
+        case 1:
+            
+            printf("\nO atributo escolhido foi: População\nVamos Comparar");
+            printf("\nCarta 1 - %s: tem uma população de %d pessoas",cidade1,populacao1);
+            printf("\nCarta 2 - %s: tem uma população de %d pessoas",cidade2,populacao2);
+
+            if (WinPontuacaoPopulacao == 1)
+            {
+                printf("\nResultado: %s (%s) Venceu",WinCartaPopulacao,cidade1);
+            }else if(WinCartaPopulacao == 2){
+                printf("\nResultado: Nós temos um empate!");
+            }else{
+                printf("\nResultado: %s (%s) Venceu",WinCartaPopulacao,cidade2);
+            }
+
+            break;
+        case 2:
+            
+            printf("\nO atributo escolhido foi: Área\nVamos Comparar");
+            printf("\nCarta 1 - %s: tem uma área de %.2fKm²",cidade1,KMquadrado1);
+            printf("\nCarta 2 - %s: tem uma área de %.2fKm²",cidade2,KMquadrado2);
+
+            if (WinPontuacaoKMquadrado == 1)
+            {
+                printf("\nResultado: %s (%s) Venceu",WinCartaKMquadrado,cidade1);
+            }else if(WinCartaPopulacao == 2){
+                printf("\nResultado: Nós temos um empate!");
+            }else{
+                printf("\nResultado: %s (%s) Venceu",WinCartaKMquadrado,cidade2);
+            }
+
+            break;
+        case 3:            
+
+            printf("\nO atributo escolhido foi: PIB\nVamos Comparar");
+            printf("\nCarta 1 - %s: tem um PIB de R$%.2f bilhões",cidade1,pib1);
+            printf("\nCarta 2 - %s: tem um PIB de R$%.2f bilhões",cidade2,pib2);
+
+            if (WinPontuacaoPIB == 1)
+            {
+                printf("\nResultado: %s (%s) Venceu",WinCartaPIB,cidade1);
+            }else if(WinPontuacaoPIB == 2){
+                printf("\nResultado: Nós temos um empate!");
+            }else{
+                printf("\nResultado: %s (%s) Venceu",WinCartaPIB,cidade2);
+            }
+
+            break;
+        case 4:
+
+            printf("\nO atributo escolhido foi: Pontos Turisticos\nVamos Comparar");
+            printf("\nCarta 1 - %s: tem um total de %d pontos turisticos",cidade1,pontos_turisticos1);
+            printf("\nCarta 2 - %s: tem um total de %d pontos turisticos",cidade2,pontos_turisticos2);
+
+            if (WinPontuacaoPontosT == 1)
+            {
+                printf("\nResultado: %s (%s) Venceu",WinCartaPontosT,cidade1);
+            }else if(WinPontuacaoPontosT == 2){
+                printf("\nResultado: Nós temos um empate!");
+            }else{
+                printf("\nResultado: %s (%s) Venceu",WinCartaPontosT,cidade2);
+            }
+
+            break;
+        case 5:
+
+            printf("\nO atributo escolhido foi: Densidade Populacional\nVamos Comparar");
+            printf("\nCarta 1 - %s: tem um total de %.2fhab/km²",cidade1,densidadep1);
+            printf("\nCarta 2 - %s: tem um total de %.2fhab/km²",cidade2,densidadep2);
+
+            if (WinPontuacaoDensidade == 1)
+            {
+                printf("\nResultado: %s (%s) Venceu",WinCartaDensidade,cidade1);
+            }else if(WinPontuacaoDensidade == 2){
+                printf("\nResultado: Nós temos um empate!");
+            }else{
+                printf("\nResultado: %s (%s) Venceu",WinCartaDensidade,cidade2);
+            }
+
+            break;
+        default:
+            printf("\nOpção invalida, digite um numero de 1 a 5 sendo:\n");
+        }
+
+    } while (opcao < 1 || opcao > 5);
+    
+    //------------------------------------------------------------------------------
+
+    //Finalização
+    printf("\n\nMuito Obrigado por jogar conosco");
+    printf("\nAté mais!");
+
+    printf("\n\n"); //Pular linha de forma estética
+    return 0;
+}
+
+/*  -----------Comentário Atividade Novato e Aventureiro Tema 2-------------------
+
+    -------A baixo está a entrada de dados usada no tema 2-------------
+
+        // Entrada de dados primeira carta
     printf("\nIniciaremos cadastrando a carta nº1");
     printf("\nQual nome da cidade que deseja cadastrar: \n");
     //scanf("%s",cidade1); -- antiga entrada de dados do tipo string para cidade1
@@ -115,63 +267,6 @@ int main()
     printf("\nAgora ultima pergunta. Quantos pontos turisticos tem essa cidade?\n");
     scanf("%d",&pontos_turisticos2);
 
-    //------------------------------------------------------------------------------
-    //Calculos
-
-    // Caclulo de densidade
-    densidadep1 = divisao(populacao1,KMquadrado1);
-    densidadep2 = divisao(populacao2,KMquadrado2);
-
-    // Calculo de PIB Percapita
-    pibpercapita1 = pibbilhao1/ (float) populacao1;
-    pibpercapita2 = pibbilhao2/ (float) populacao2;
-
-    // Calculo de super poder
-    super1 = (float)populacao1+(float)pontos_turisticos1+KMquadrado1+pib1+pibpercapita1+inversodensidade1;
-    super2 = (float)populacao2+(float)pontos_turisticos2+KMquadrado2+pib2+pibpercapita2+inversodensidade2; 
-
-    //Comparação das Cartas
-    int WinPontuacaoPopulacao = maiorganha(populacao1,populacao2);
-    int WinPontuacaoKMquadrado = maiorganha(KMquadrado1,KMquadrado2);
-    int WinPontuacaoPIB = maiorganha(pibbilhao1,pibbilhao2);
-    int WinPontuacaoPontosT = maiorganha(pontos_turisticos1,pontos_turisticos2);
-    int WinPontuacaoDensidade = menorganha(densidadep1,densidadep2);
-    int WinPontuacaoPIBPerc = maiorganha(pibpercapita1,pibpercapita2);
-    int WinPontuacaoSuper = maiorganha(super1,super2);
-
-    //Definicição de vencedores
-    char *WinCartaPopulacao = carta(WinPontuacaoPopulacao);
-    char *WinCartaKMquadrado = carta(WinPontuacaoKMquadrado);
-    char *WinCartaPIB = carta(WinPontuacaoPIB);
-    char *WinCartaPontosT = carta(WinPontuacaoPontosT);
-    char *WinCartaDensidade = carta(WinPontuacaoDensidade);
-    char *WinCartaPIBPerc = carta(WinPontuacaoPIBPerc);
-    char *WinCartaSuper = carta(WinPontuacaoSuper);
-
-    //------------------------------------------------------------------------------
-
-    // Mostrar Vencedores 
-    printf("\n\n");
-    printf("\nComparação das Cartas (Atributo: Pontos Turisticos):");
-    printf("\nCarta 1 - %s: %d tem pontos turisticos",cidade1,pontos_turisticos1);
-    printf("\nCarta 2 - %s: %d tem pontos turisticos",cidade2,pontos_turisticos2);
-
-    if (WinPontuacaoPontosT == 1)
-    {
-        printf("\nResultado: %s (%s) Venceu",WinCartaPontosT,cidade1);
-    }else{
-        printf("\nResultado: %s (%s) Venceu",WinCartaPontosT,cidade2);
-    }
-
-    //Finalização
-    printf("\n\nMuito Obrigado por jogar conosco");
-    printf("\nAté mais!");
-
-    printf("\n\n"); //Pular linha de forma estética
-    return 0;
-}
-
-/*  -----------Comentário Atividade Novato e Aventureiro Tema 2-------------------
     ------- A baixo está a demonstração final da atividade notavo de aventureiro do tema 2 ----------
 
     printf("\n\nOlhe a baixo as cartas que cadastrou");
